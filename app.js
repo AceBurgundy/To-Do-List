c53ced20bd0f07e656afb37e3920b4dca42f5f6e
AceBurgundy <Samadriansabalo99@gmail.com>
2022-11-28 17:31:49 +0800

    used ejs for templating

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const bodyParser = require("body-parser")
 
 const app = express();
 
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//use ejs templating engine
+app.set("view engine", "ejs")
+
+const list = []
+
 app.get("/", (request, response) => {
-    response.send("Hello")
+    response.render("list", { list: list, listAmmount: list.length })
+})
+
+app.post("/", (request, response) => {
+    const todo = request.body.todo
+    list.push(todo)
+    response.redirect("/")
+})
+
+app.post("/clear", (request, response) => {
+    list.length = 0
+    response.redirect("/")
 })
 
 app.listen(3000, () => {
